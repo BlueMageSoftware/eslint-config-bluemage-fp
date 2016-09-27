0b24e7ee35904ce8aa77a12ece880d9e6f882bcd
Noris Alsaadie <noris@bluemagesoftware.com>
2016-09-27 22:50:03 +0100

    Initial version

---
+++ airbnb-overrides.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "browser": false
+  },
+  "extends": "airbnb",
+  "rules": {
+    "comma-dangle": ["error", "never"],
+    "semi": ["error", "never"]
+  }
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "browser": false
+  },
+  "plugins": [
+    "fp"
+  ],
+  "extends": [
+    "./airbnb-overrides.js",
+    "plugin:fp/recommended"
+  ],
+  "rules": {}
+}
